7bd1b27d2106428d9199b12a5c7e6ad57a3c5804
jonitaajamo <joni.taajamo@outlook.com>
2019-04-08 23:30:39 +0300

    otettu checkstyle käyttöön

--- TelegramEventBot/src/main/java/telegrameventbot/domain/TelegramEventBot.java
+++ TelegramEventBot/src/main/java/telegrameventbot/domain/TelegramEventBot.java
@@ -16,18 +16,18 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
  * @author jonitaajamo
  */
 public class TelegramEventBot extends TelegramLongPollingBot {
-    
+
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             Message message = update.getMessage();
             String messageText = message.getText();
             long chatId = update.getMessage().getChatId();
-            
+
             readCommand(messageText, chatId);
         }
     }
-    
+
     @Override
     public String getBotUsername() {
         return "PrimitiveEventBot";
@@ -49,22 +49,22 @@ public class TelegramEventBot extends TelegramLongPollingBot {
         }
         return null;
     }
-    
+
     public String readCommand(String messageText, long chatId) {
         String command = messageText.split(" ")[0];
         String answer = "Something went terribly wrong.";
 
-            if (command.startsWith("/")) {
-                switch (command) {
-                    case "/addevent":
-                        answer = "Tried to add event, but failed";
-                    case "/attend":
-                        answer = "Tried to attend event, but failed";
-                }
-            } else {
-                answer = "Thats not a command";
+        if (command.startsWith("/")) {
+            switch (command) {
+                case "/addevent":
+                    answer = "Tried to add event, but failed";
+                case "/attend":
+                    answer = "Tried to attend event, but failed";
             }
-        
+        } else {
+            answer = "Thats not a command";
+        }
+
         sendMessage(answer, chatId);
         return answer;
     }
